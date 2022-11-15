18bcf8db1713e2cec9003e31dafdab1a7f9f98fb
Maël Billaud <billaud.mael@gmail.com>
2022-11-15 07:52:42 +0100

    Change cursor style

--- js/niveaux.js
+++ js/niveaux.js
@@ -11,19 +11,28 @@ function setLockLevel() {
     if(points < 2) {
         lock2.setAttribute("class", "fa-solid fa-lock");
         level2.setAttribute("onclick", "");
+        level2.setAttribute("class", "lock");
+
         lock3.setAttribute("class", "fa-solid fa-lock");
         level3.setAttribute("onclick", "");
+        level3.setAttribute("class", "lock");
     } else {
         if(points < 4) {
             lock2.setAttribute("class", "");
             level2.setAttribute("onclick", "window.location='niveau2.html'");
+            level2.setAttribute("class", "");
+
             lock3.setAttribute("class", "fa-solid fa-lock");
             level3.setAttribute("onclick", "");
+            level3.setAttribute("class", "lock");
         } else {
             lock2.setAttribute("class", "");
             level2.setAttribute("onclick", "window.location='niveau2.html'");
+            level2.setAttribute("class", "");
+
             lock3.setAttribute("class", "");
             level3.setAttribute("onclick", "window.location='niveau3.html'");
+            level3.setAttribute("class", "");
         }
     }
     
